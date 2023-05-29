b0eefe58e32b9469e3caa836934f195757071e14
gabrielduquesne <gabriel.reolon@edu.unipar.br>
2023-05-29 08:45:27 -0300

    Banco

--- src/main/java/br/eu/banco/Banco.java
+++ src/main/java/br/eu/banco/Banco.java
@@ -1,16 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.eu.banco;
 
-/**
- *
- * @author dukis
- */
+import br.eu.banco.newpackage.ContaBanco;
+
+
 public class Banco {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ContaBanco p1 =  new ContaBanco ();
+        p1.setNmrConta( 12344);
+        p1.setDono("Xandão o  grande");
+        p1.abrirConta("CC");
+        p1.estadoAtual();
+     
+        ContaBanco p2 = new ContaBanco();
+        p2.setNmrConta(  312435);
+        p2.setDono("Julia");
+        p2.abrirConta("CP");
+        p2.estadoAtual();
     }
 }
